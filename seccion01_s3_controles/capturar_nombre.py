1d32ae7137b8428c3a2fda35656526373bdbc00f
Williams Tuate <89873748+overwillys@users.noreply.github.com>
2024-01-29 17:40:04 -0300

    Update saludo , mostrando en la misma pantalla

--- seccion01_s3_controles/capturar_nombre.py
+++ seccion01_s3_controles/capturar_nombre.py
@@ -5,15 +5,18 @@ import flet as ft
 
 def main(page: ft.Page):
     
-    txt_nombre = ft.TextField(label="Digite tu nombre")
+    txt_nombre = ft.TextField(label="Digite tu nombre")    
+    lbl_saludo = ft.Text() 
     
     def Saludar(event):
-        print(f'Hola...{txt_nombre.value}!')
+        lbl_saludo.value = f'Hola...{txt_nombre.value}!'
+        page.update()
         
     row = ft.Row(controls=[
         #ft.TextField(label="Digite su nombre"),
         txt_nombre,
-        ft.ElevatedButton(text="Saludar", on_click=Saludar)
+        ft.ElevatedButton(text="Saludar", on_click=Saludar),
+        lbl_saludo
         
     ])
     
