783397730b3368674a2b9d4a6b99339917817f62
Williams Tuate <89873748+overwillys@users.noreply.github.com>
2024-01-29 16:57:31 -0300

    Update container - Otra forma de mostrar datos

--- seccion01_s3_controles/container.py
+++ seccion01_s3_controles/container.py
@@ -1,15 +1,21 @@
 import flet as ft
 
 
+# def main(page: ft.Page):
+#     page.add(
+#         ft.Row(
+#             controls=[
+#                 ft.Text("A"),
+#                 ft.Text("B"),
+#                 ft.Text("C"),
+#             ]
+#         )
+#     )
+
+#Otra manera de crear lo mismo 
+
 def main(page: ft.Page):
-    page.add(
-        ft.Row(
-            controls=[
-                ft.Text("A"),
-                ft.Text("B"),
-                ft.Text("C"),
-            ]
-        )
-    )
+    row_datos = ft.Row(controls=[ft.Text('Python'), ft.Text('Flet'), ft.Text('Flutter')])
+    page.add(row_datos)
 
 ft.app(target=main, view=ft.AppView.WEB_BROWSER)
